3268f2ede042e2d88e5453b18d3c9a3bd1542bc1
Laivy <dnlfg.contato@gmail.com>
2024-02-19 00:40:37 -0300

    Added ftp and socks proxies modules (to-do)

---
+++ ftp/src/main/java/codes/laivy/proxy/Main.java
@@ -0,0 +1,7 @@
+package codes.laivy.proxy;
+
+// todo: ftp proxy
+public final class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ socks/src/main/java/codes/laivy/proxy/Main.java
@@ -0,0 +1,7 @@
+package codes.laivy.proxy;
+
+// todo: socks proxy
+public final class Main {
+    public static void main(String[] args) {
+    }
+}
