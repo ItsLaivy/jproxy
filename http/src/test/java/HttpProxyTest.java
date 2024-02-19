21b25417421480ec5d57962d4bedefb7ddea8c5e
Laivy <dnlfg.contato@gmail.com>
2024-02-19 17:55:31 -0300

    Improved tests

--- http/src/test/java/HttpProxyTest.java
+++ http/src/test/java/HttpProxyTest.java
@@ -19,15 +19,16 @@ public final class HttpProxyTest {
     @Test
     public void connectReconnect() throws Throwable {
         // Start native http proxy
-        @NotNull HttpProxy proxy = HttpProxy.create(PROXY_ADDRESS, null);
-        Assertions.assertTrue(proxy.start());
+        try (@NotNull HttpProxy proxy = HttpProxy.create(PROXY_ADDRESS, null)) {
+            Assertions.assertTrue(proxy.start());
 
-        // End activities and stop
-        Assertions.assertTrue(proxy.stop());
-        // Start activities again
-        Assertions.assertTrue(proxy.start());
-        // Finally end activities and stop without starting again
-        Assertions.assertTrue(proxy.stop());
+            // End activities and stop
+            Assertions.assertTrue(proxy.stop());
+            // Start activities again
+            Assertions.assertTrue(proxy.start());
+            // Finally end activities and stop without starting again
+            Assertions.assertTrue(proxy.stop());
+        }
     }
 
     @Nested
@@ -35,14 +36,85 @@ public final class HttpProxyTest {
         @Test
         public void get() throws Throwable {
             // Start native http proxy
-            @NotNull HttpProxy proxy = HttpProxy.create(PROXY_ADDRESS, null);
-            Assertions.assertTrue(proxy.start());
+            try (@NotNull HttpProxy proxy = HttpProxy.create(PROXY_ADDRESS, null)) {
+                Assertions.assertTrue(proxy.start());
+
+                // Test with JSoup
+                @NotNull Connection connection = Jsoup.connect("http://localhost/")
+                        .proxy(proxy)
+
+                        .method(Connection.Method.GET)
+
+                        .ignoreContentType(true)
+                        .ignoreHttpErrors(true);
+                @NotNull Connection.Response response = connection.execute();
+                Assertions.assertEquals(HttpStatus.SC_OK, response.statusCode(), response.statusMessage());
+
+                // End activities and stop
+                Assertions.assertTrue(proxy.stop());
+            }
+        }
+        @Test
+        public void post() throws Throwable {
+            // Start native http proxy
+            try (@NotNull HttpProxy proxy = HttpProxy.create(PROXY_ADDRESS, null)) {
+                Assertions.assertTrue(proxy.start());
+
+                // Json data
+                @NotNull String data = "{\"test\":\"data\"}";
+                // Test with JSoup
+                @NotNull Connection connection = Jsoup.connect("http://localhost/")
+                        .proxy(proxy)
+
+                        .method(Connection.Method.POST)
+                        .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .requestBody(data)
+
+                        .ignoreContentType(true)
+                        .ignoreHttpErrors(true);
+                @NotNull Connection.Response response = connection.execute();
+                Assertions.assertEquals(HttpStatus.SC_OK, response.statusCode(), response.statusMessage());
+
+                // End activities and stop
+                Assertions.assertTrue(proxy.stop());
+            }
+        }
+        @Test
+        public void put() throws Throwable {
+            // Start native http proxy
+            try (@NotNull HttpProxy proxy = HttpProxy.create(PROXY_ADDRESS, null)) {
+                Assertions.assertTrue(proxy.start());
+
+                // Json data
+                @NotNull String data = "{\"test\":\"data\"}";
+                // Test with JSoup
+                @NotNull Connection connection = Jsoup.connect("http://localhost/")
+                        .proxy(proxy)
+
+                        .method(Connection.Method.PUT)
+                        .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .requestBody(data)
+
+                        .ignoreContentType(true)
+                        .ignoreHttpErrors(true);
+                @NotNull Connection.Response response = connection.execute();
+                Assertions.assertEquals(HttpStatus.SC_OK, response.statusCode(), response.statusMessage());
+
+                // End activities and stop
+                Assertions.assertTrue(proxy.stop());
+            }
+        }
+        @Test
+        public void delete() throws Throwable {
+            // Start native http proxy
+            try (@NotNull HttpProxy proxy = HttpProxy.create(PROXY_ADDRESS, null)) {
+                Assertions.assertTrue(proxy.start());
 
             // Test with JSoup
             @NotNull Connection connection = Jsoup.connect("http://localhost/")
                     .proxy(proxy)
 
-                    .method(Connection.Method.GET)
+                    .method(Connection.Method.DELETE)
 
                     .ignoreContentType(true)
                     .ignoreHttpErrors(true);
@@ -52,29 +124,48 @@ public final class HttpProxyTest {
             // End activities and stop
             Assertions.assertTrue(proxy.stop());
         }
+        }
         @Test
-        public void post() throws Throwable {
+        public void head() throws Throwable {
             // Start native http proxy
-            @NotNull HttpProxy proxy = HttpProxy.create(PROXY_ADDRESS, null);
-            Assertions.assertTrue(proxy.start());
+            try (@NotNull HttpProxy proxy = HttpProxy.create(PROXY_ADDRESS, null)) {
+                Assertions.assertTrue(proxy.start());
 
-            // Json data
-            @NotNull String data = "{\"test\":\"data\"}";
-            // Test with JSoup
-            @NotNull Connection connection = Jsoup.connect("http://localhost/")
-                    .proxy(proxy)
+                // Test with JSoup
+                @NotNull Connection connection = Jsoup.connect("http://localhost/")
+                        .proxy(proxy)
 
-                    .method(Connection.Method.POST)
-                    .header(HttpHeaders.CONTENT_TYPE, "application/json")
-                    .requestBody(data)
+                        .method(Connection.Method.HEAD)
 
-                    .ignoreContentType(true)
-                    .ignoreHttpErrors(true);
-            @NotNull Connection.Response response = connection.execute();
-            Assertions.assertEquals(HttpStatus.SC_OK, response.statusCode(), response.statusMessage());
+                        .ignoreContentType(true)
+                        .ignoreHttpErrors(true);
+                @NotNull Connection.Response response = connection.execute();
+                Assertions.assertEquals(HttpStatus.SC_OK, response.statusCode(), response.statusMessage());
 
-            // End activities and stop
-            Assertions.assertTrue(proxy.stop());
+                // End activities and stop
+                Assertions.assertTrue(proxy.stop());
+            }
+        }
+        @Test
+        public void options() throws Throwable {
+            // Start native http proxy
+            try (@NotNull HttpProxy proxy = HttpProxy.create(PROXY_ADDRESS, null)) {
+                Assertions.assertTrue(proxy.start());
+
+                // Test with JSoup
+                @NotNull Connection connection = Jsoup.connect("http://localhost/")
+                        .proxy(proxy)
+
+                        .method(Connection.Method.OPTIONS)
+
+                        .ignoreContentType(true)
+                        .ignoreHttpErrors(true);
+                @NotNull Connection.Response response = connection.execute();
+                Assertions.assertEquals(HttpStatus.SC_OK, response.statusCode(), response.statusMessage());
+
+                // End activities and stop
+                Assertions.assertTrue(proxy.stop());
+            }
         }
 
         @Test
@@ -86,21 +177,22 @@ public final class HttpProxyTest {
 
             // Prepare connection and start http proxy
             @NotNull Connection connection;
-            @NotNull HttpProxy proxy = HttpProxy.create(PROXY_ADDRESS, Authorization.bearer(headerName, string -> string.equals(validToken)));
-            Assertions.assertTrue(proxy.start());
+            try (@NotNull HttpProxy proxy = HttpProxy.create(PROXY_ADDRESS, Authorization.bearer("Proxy-Authorization", (string) -> string.equals(validToken)))) {
+                Assertions.assertTrue(proxy.start());
 
-            // Test with JSoup without authorization
-            connection = Jsoup.connect("http://localhost/").proxy(proxy).ignoreContentType(true).ignoreHttpErrors(true);
-            Assertions.assertEquals(HttpStatus.SC_UNAUTHORIZED, connection.execute().statusCode());
-            // Test with JSoup with invalid authorization
-            connection = Jsoup.connect("http://localhost/").proxy(proxy).header(headerName, "Bearer " + invalidToken).ignoreContentType(true).ignoreHttpErrors(true);
-            Assertions.assertEquals(HttpStatus.SC_UNAUTHORIZED, connection.execute().statusCode());
-            // Test with JSoup with valid authorization
-            connection = Jsoup.connect("http://localhost/").proxy(proxy).header(headerName, "Bearer " + validToken).ignoreContentType(true).ignoreHttpErrors(true);
-            Assertions.assertEquals(HttpStatus.SC_OK, connection.execute().statusCode());
+                // Test with JSoup without authorization
+                connection = Jsoup.connect("http://localhost/").proxy(proxy).ignoreContentType(true).ignoreHttpErrors(true);
+                Assertions.assertEquals(HttpStatus.SC_UNAUTHORIZED, connection.execute().statusCode());
+                // Test with JSoup with invalid authorization
+                connection = Jsoup.connect("http://localhost/").proxy(proxy).header(headerName, "Bearer " + invalidToken).ignoreContentType(true).ignoreHttpErrors(true);
+                Assertions.assertEquals(HttpStatus.SC_UNAUTHORIZED, connection.execute().statusCode());
+                // Test with JSoup with valid authorization
+                connection = Jsoup.connect("http://localhost/").proxy(proxy).header(headerName, "Bearer " + validToken).ignoreContentType(true).ignoreHttpErrors(true);
+                Assertions.assertEquals(HttpStatus.SC_OK, connection.execute().statusCode());
 
-            // End activities and stop
-            Assertions.assertTrue(proxy.stop());
+                // End activities and stop
+                Assertions.assertTrue(proxy.stop());
+            }
         }
     }
 
