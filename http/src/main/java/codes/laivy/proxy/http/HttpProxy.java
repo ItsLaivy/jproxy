d69a5ef1274df696159e187b9e126f2aca66c9ce
Laivy <dnlfg.contato@gmail.com>
2024-02-21 16:58:15 -0300

    Improved http proxy main class

--- http/src/main/java/codes/laivy/proxy/http/HttpProxy.java
+++ http/src/main/java/codes/laivy/proxy/http/HttpProxy.java
@@ -2,8 +2,8 @@ package codes.laivy.proxy.http;
 
 import codes.laivy.proxy.ProxyServer;
 import codes.laivy.proxy.http.connection.HttpProxyClient;
+import codes.laivy.proxy.http.core.HttpAuthorization;
 import codes.laivy.proxy.http.impl.HttpProxyImpl;
-import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
 import org.apache.hc.core5.http.HttpException;
 import org.apache.hc.core5.http.HttpRequest;
 import org.apache.hc.core5.http.HttpResponse;
@@ -12,12 +12,9 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
-import java.net.*;
-import java.util.Arrays;
-import java.util.Base64;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.util.Collection;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public abstract class HttpProxy extends ProxyServer implements AutoCloseable {
 
@@ -26,15 +23,15 @@ public abstract class HttpProxy extends ProxyServer implements AutoCloseable {
     public static void main(String[] args) {
     }
 
-    public static @NotNull HttpProxy create(@NotNull InetSocketAddress address, @Nullable Authorization authorization) throws IOException {
+    public static @NotNull HttpProxy create(@NotNull InetSocketAddress address, @Nullable HttpAuthorization authorization) throws IOException {
         return new HttpProxyImpl(address, authorization);
     }
 
     // Object
 
-    private final @Nullable Authorization authorization;
+    private final @Nullable HttpAuthorization authorization;
 
-    protected HttpProxy(@NotNull InetSocketAddress address, @Nullable Authorization authorization) {
+    protected HttpProxy(@NotNull InetSocketAddress address, @Nullable HttpAuthorization authorization) {
         super(Type.HTTP, address);
         this.authorization = authorization;
     }
@@ -50,7 +47,7 @@ public abstract class HttpProxy extends ProxyServer implements AutoCloseable {
      * A proxy authentication can be used to allow only certain users. If the authentication is null, the user who makes a request using it will not need to provide the authentication details
      * @return the authentication object or null if none is required
      */
-    public @Nullable Authorization getAuthentication() {
+    public @Nullable HttpAuthorization getAuthentication() {
         return this.authorization;
     }
 
@@ -89,106 +86,5 @@ public abstract class HttpProxy extends ProxyServer implements AutoCloseable {
     }
 
     // Classes
-
-    /**
-     * The authorization class is used to allow only users who provide some degree of authentication to use the proxy
-     * @since 1.0-SNAPSHOT
-     */
-    public interface Authorization {
-
-        /**
-         * Creates an authorization object that uses the bearer token scheme
-         * <p style="color:red">Note: After the first successfully validation, it removes the authorization header. It means if you try to validate again, it will return false.</p>
-         *
-         * @since 1.0-SNAPSHOT
-         * @author Daniel Richard (Laivy)
-         *
-         * @see <a href="https://apidog.com/articles/what-is-bearer-token/">Bearer Authorization</a>
-         * @param predicate a function that checks if the token is valid
-         * @return an authentication object that implements the bearer token logic
-         */
-        static @NotNull Authorization bearer(final @NotNull String headerName, @NotNull Predicate<String> predicate) {
-            return (socket, request) -> {
-                try {
-                    if (!request.containsHeader(headerName)) {
-                        return false;
-                    }
-
-                    @NotNull String[] auth = request.getLastHeader(headerName).getValue().split(" ");
-
-                    if (auth.length < 2) {
-                        return false;
-                    } else if (!auth[0].equalsIgnoreCase("Bearer")) {
-                        return false;
-                    }
-
-                    int row = 0;
-                    @NotNull StringBuilder merged = new StringBuilder();
-                    for (@NotNull String part : Arrays.stream(auth).skip(1).toArray(String[]::new)) {
-                        if (row > 0) merged.append(" ");
-                        merged.append(part);
-                        row++;
-                    }
-
-                    return predicate.test(merged.toString());
-                } catch (@NotNull Throwable ignore) {
-                    return false;
-                } finally {
-                    request.removeHeaders(headerName);
-                }
-            };
-        }
-
-        /**
-         * Creates an authorization object that uses the basic token scheme
-         * <p style="color:red">Note: After the first successfully validation, it removes the authorization header. It means if you try to validate again, it will return false.</p>
-         *
-         * @since 1.0-SNAPSHOT
-         * @author Daniel Richard (Laivy)
-         *
-         * @see <a href="https://en.wikipedia.org/wiki/Basic_access_authentication">Basic Authorization</a>
-         * @param predicate a function that checks if the token is valid
-         * @return an authentication object that implements the bearer token logic
-         */
-        static @NotNull Authorization basic(final @NotNull String headerName, @NotNull Predicate<UsernamePasswordCredentials> predicate) {
-            return (socket, request) -> {
-                try {
-                    if (!request.containsHeader(headerName)) {
-                        return false;
-                    }
-
-                    @NotNull String[] auth = request.getLastHeader(headerName).getValue().split(" ");
-
-                    if (auth.length < 2) {
-                        return false;
-                    } else if (!auth[0].equalsIgnoreCase("Basic")) {
-                        return false;
-                    }
-
-                    @NotNull String encoded = Arrays.stream(auth).skip(1).map(string -> string + " ").collect(Collectors.joining());
-                    @NotNull String[] decoded = new String(Base64.getDecoder().decode(encoded)).split(":");
-
-                    return predicate.test(new UsernamePasswordCredentials(decoded[0], decoded[1].toCharArray()));
-                } catch (@NotNull Throwable ignore) {
-                    return false;
-                } finally {
-                    request.removeHeaders(headerName);
-                }
-            };
-        }
-
-        /**
-         * Validates the authorization by analyzing the socket and the request, returns true if the authentication was approved and the user is able to use the proxy or false otherwise. This method is invoked whenever a new request is made
-         *
-         * @since 1.0-SNAPSHOT
-         * @author Daniel Richard (Laivy)
-         *
-         * @param socket the socket that connects the client and the proxy
-         * @param request the HTTP request sent by the client
-         * @return true if the authentication is valid, false otherwise
-         */
-        boolean validate(@NotNull Socket socket, @NotNull HttpRequest request);
-
-    }
 
 }
