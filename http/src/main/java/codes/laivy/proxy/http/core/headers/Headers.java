8af4b59cec64246698fe62b74ed380e00c2b587e
Laivy <dnlfg.contato@gmail.com>
2024-02-23 16:00:33 -0300

    Added headers and header tests

--- http/src/main/java/codes/laivy/proxy/http/core/headers/Headers.java
+++ http/src/main/java/codes/laivy/proxy/http/core/headers/Headers.java
@@ -3,6 +3,7 @@ package codes.laivy.proxy.http.core.headers;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -16,6 +17,23 @@ public interface Headers extends Iterable<Header> {
     static @NotNull Headers createImmutable(@NotNull Header @NotNull [] headers) {
         return new ImmutableHeadersProvider(headers);
     }
+
+    // Provided
+
+    /**
+     * @see <a href="regexr.com/7sfol">RegExr Tests</a>
+     * @apiNote Last change: 23/02/2024 | 15:47 (GMT-3)
+     */
+    @NotNull HeaderKey ACCEPT = HeaderKey.create("Accept", Pattern.compile("^([a-zA-Z0-9+.*]+/[a-zA-Z0-9+.*]+(; ?q=[0-1](\\.[0-9]{1,2})?)?(, *)?)+$"));
+
+    @NotNull HeaderKey ACCEPT_CH = HeaderKey.create("Accept-CH");
+    @NotNull HeaderKey ACCEPT_CH_LIFETIME = HeaderKey.create("Accept-CH-Lifetime", Pattern.compile("^\\d+$"));
+
+    /**
+     * @see <a href="regexr.com/7sfpg">RegExr Tests</a>
+     * @apiNote Last change: 23/02/2024 | 15:58 (GMT-3)
+     */
+    @NotNull HeaderKey ACCEPT_CHARSET = HeaderKey.create("Accept-Charset", Pattern.compile("^([\\w\\-]+(; ?q=[0-1]\\.?[0-9](\\.[0-9]{1,2})?)?(, *)?)+$"));
 
     // Object
 
