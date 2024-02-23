5bb9458da394f888b03021944f9abb14aadc43fe
Laivy <dnlfg.contato@gmail.com>
2024-02-23 03:58:25 -0300

    Added name value pair creator static method

--- http/src/main/java/codes/laivy/proxy/http/core/NameValuePair.java
+++ http/src/main/java/codes/laivy/proxy/http/core/NameValuePair.java
@@ -1,10 +1,26 @@
 package codes.laivy.proxy.http.core;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.UnknownNullability;
 
 public interface NameValuePair {
 
     @UnknownNullability String getName();
     @UnknownNullability String getValue();
+
+    static @NotNull NameValuePair create(final @Nullable String name, final @Nullable String value) {
+        return new NameValuePair() {
+            @Override
+            public @UnknownNullability String getName() {
+                return name;
+            }
+
+            @Override
+            public @UnknownNullability String getValue() {
+                return value;
+            }
+        };
+    }
 
 }
